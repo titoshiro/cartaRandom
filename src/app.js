66a76d5d0734184fa46cc44aefe6030de0517259
Your Name <you@example.com>
2023-05-19 19:20:41 -0400

    cartaRamdon

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function numeroAleatorio(){
+let num =['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+let numAleatorio = num[Math.floor(Math.random()*num.length)];
+return numAleatorio
+}
+
+function pintaAleatorio(){
+  let pinta=['♥', '♦', '♣', '♠'];
+  let pintaAleatorio= pinta[Math.floor(Math.random()*pinta.length)];
+  return pintaAleatorio
+  }
+  
+
+
+function generadorDeCartas() {
+  let numero = numeroAleatorio();
+  let pinta= pintaAleatorio();
+  document.getElementById('numerotexto').textContent = numero;
+  document.getElementById("pinta1").textContent=pinta
+  document.getElementById("pinta2").textContent=pinta
+}
+
